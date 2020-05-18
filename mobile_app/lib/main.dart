b3e86154b81bebd21c3ba35b0b04fe180fb4fc36
Gurleen Sethi <sarusethi@rocketmail.com>
2020-05-17 23:00:44 -0400

    Initial Commit :P

---
+++ mobile_app/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+import 'dart:io';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _textEditingController = TextEditingController();
+  File _dataFile;
+
+  void _updateFile() async {
+    if (_dataFile != null) {
+      await _dataFile.writeAsString(_textEditingController.text);
+    }
+  }
+
+  void _initializeFile() async {
+    final tempDir = await getApplicationDocumentsDirectory();
+    _dataFile = File(path.join(tempDir.path, "data.txt"));
+    if (!(await _dataFile.exists())) {
+      await _dataFile.create();
+    }
+    final data = await _dataFile.readAsString();
+    _textEditingController.text = data;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeFile();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _textEditingController,
+                decoration: InputDecoration(
+                  hintText: 'Enter file data',
+                ),
+              ),
+              RaisedButton(
+                child: Text('Update file'),
+                onPressed: _updateFile,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
