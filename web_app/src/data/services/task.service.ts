396c8d7bc68ed02f78831aa2e47c5e60684adb1c
Gurleen Sethi <sarusethi@rocketmail.com>
2020-05-19 22:26:10 -0400

    feat: add methods to create, delete and get one task in TaskService

--- web_app/src/data/services/task.service.ts
+++ web_app/src/data/services/task.service.ts
@@ -1,9 +1,33 @@
-import { Task } from "src/data/types";
+import { Task, CreateTaskData } from "src/data/types";
 
 export class TaskService {
   private tasks: Task[] = [];
 
-  public async getAll(): Promise<Task[]> {
-    return this.tasks;
+  public async getAll(
+    parentId: number | undefined = undefined
+  ): Promise<Task[]> {
+    return this.tasks.filter((task) => task.parentId === parentId);
+  }
+
+  public async getById(id: number): Promise<Task | undefined> {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  public async createTask(data: CreateTaskData): Promise<Task> {
+    const task: Task = {
+      ...data,
+      createdAt: Date.now(),
+      id: this.tasks.length + 1,
+      isComplete: false,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  public async deleteTask(task: Task): Promise<void> {
+    const index = this.tasks.findIndex((t) => t.id === task.id);
+    if (index >= 0) {
+      this.tasks.splice(index, 1);
+    }
   }
 }
