fac2868fa2eaae9d57814dd5c4bf74f2cd3267d2
Gurleen Sethi <sarusethi@rocketmail.com>
2020-05-25 23:17:30 -0400

    refactor: Move styles from index.css to App.css.

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -3,16 +3,24 @@ import { HashRouter, Route } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { Provider } from "react-redux";
 import { createAppStore } from "./data/redux/store.redux";
+import "./App.css";
 
 const store = createAppStore();
 
+const getBasename = () => {
+  if (process.env.NODE_ENV === "production") {
+    return process.env.PUBLIC_URL + "/";
+  }
+  return "/";
+};
+
 const App: FunctionComponent = () => {
   return (
-    <Provider store={store}>
-      <HashRouter basename={process.env.PUBLIC_URL + "/"}>
+    <HashRouter basename={getBasename()}>
+      <Provider store={store}>
         <Route path="/" component={Home} />
-      </HashRouter>
-    </Provider>
+      </Provider>
+    </HashRouter>
   );
 };
 
