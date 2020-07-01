9d6bbf628e3a708e446757bd5a519b02368918fd
Gurleen Sethi <sarusethi@rocketmail.com>
2020-06-30 22:56:13 -0400

    feat: add type for edit task modal.

--- web_app/src/data/types/ui.ts
+++ web_app/src/data/types/ui.ts
@@ -1,5 +1,6 @@
 export const DELETE_ALL_TASKS_MODAL = "DELETE_ALL_TASKS_MODAL";
 export const SETTINGS_MODAL = "SETTINGS_MODAL";
+export const EDIT_TASK_MODAL = "EDIT_TASK_MODAL";
 
 export interface BaseModalOptions {}
 
@@ -11,6 +12,13 @@ export interface SettingsModalOptions extends BaseModalOptions {
   type: typeof SETTINGS_MODAL;
 }
 
+export interface EditTaskModalOptions extends BaseModalOptions {
+  type: typeof EDIT_TASK_MODAL;
+}
+
 export type ModalOptions = DeleteAllTaskModalOptions | SettingsModalOptions;
 
-export type ModalType = typeof DELETE_ALL_TASKS_MODAL | typeof SETTINGS_MODAL;
+export type ModalType =
+  | typeof DELETE_ALL_TASKS_MODAL
+  | typeof SETTINGS_MODAL
+  | typeof EDIT_TASK_MODAL;
