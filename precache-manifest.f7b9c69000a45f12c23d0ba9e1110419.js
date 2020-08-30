fefc8e372c06e40a973010992de0c945d5998727
gurleensethi <sarusethi@rocketmail.com>
2020-08-30 02:15:59 +0000

    Deployment of web app.

---
+++ precache-manifest.f7b9c69000a45f12c23d0ba9e1110419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827e31aeac8d14a2e5578c94d81e32b1",
+    "url": "/infinite-todo/index.html"
+  },
+  {
+    "revision": "49a8e276642096b36ad6",
+    "url": "/infinite-todo/static/css/main.d36f532e.chunk.css"
+  },
+  {
+    "revision": "f28287e06e2dfd8a5d4a",
+    "url": "/infinite-todo/static/js/2.1effb41c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/infinite-todo/static/js/2.1effb41c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a8e276642096b36ad6",
+    "url": "/infinite-todo/static/js/main.cf3f69df.chunk.js"
+  },
+  {
+    "revision": "dd2002bcaaaf52fcde89",
+    "url": "/infinite-todo/static/js/runtime-main.bd34ea4e.js"
+  }
+]);
